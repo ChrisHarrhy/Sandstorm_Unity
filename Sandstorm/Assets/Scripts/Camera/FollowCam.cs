7ee622e6ebb47d82aa4b8db218d6db0d820097c1
Chris <christian2.harrhy@live.uwe.ac.uk>
2026-07-25 13:49:47 +0100

    Character and anims imported. AnimController set up

---
+++ Sandstorm/Assets/Scripts/Camera/FollowCam.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public GameObject player;
+
+
+    private void Start()
+    {
+        player = GetComponent<GameObject>();
+    }
+
+    private void Update()
+    {
+        if (player != null)
+        {
+            Debug.Log("Player not found");
+        }
+
+        else
+        {
+            Transform.
+        }
+    }
+
+
+}
